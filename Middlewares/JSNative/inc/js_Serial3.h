bca925bfc1f59b241c7ff953507933f84d5f1407
inho <InhoChoi@inhoui-MacBook-Pro.local>
2015-10-08 16:12:02 +0900

    Uart 구현에 필요한 파일 추가

---
+++ Middlewares/JSNative/inc/js_Serial3.h
@@ -0,0 +1,7 @@
+#include "main.h"
+#include "TinyJS.h"
+extern "C"{
+	void js_Serial3_Begin(CScriptVar *v, void *userdata);
+	void js_Serial3_Print(CScriptVar *v, void *userdata);
+	void js_Serial3_Read(CScriptVar *v, void *userdata);
+}
