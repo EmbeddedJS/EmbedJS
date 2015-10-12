611509bc3673e6e06d5d258e4c9ca908946e9e98
inho <InhoChoi@inhoui-MacBook-Pro.local>
2015-10-12 17:37:30 +0900

    예제 수정

--- FlashWrite/bluetooth.js
+++ FlashWrite/bluetooth.js
@@ -1,13 +1,14 @@
 Serial3.begin(9600);
 pinMode(4,13,1);
 while(1){
-	var ch = Serial3.read();
-	if( ch != 0){
-		Serial3.print(ch);
-	}
-	
-	digitalWrite(4,13,0);
-	Delay(500);
-	digitalWrite(4,13,1);
-	Delay(500);
+		while(Serial3.available()){
+				var ch = Serial3.read();
+				if( ch != 0){
+						Serial3.print(ch);
+				}
+		}
+		digitalWrite(4,13,0);
+		Delay(500);
+		digitalWrite(4,13,1);
+		Delay(500);
 }
