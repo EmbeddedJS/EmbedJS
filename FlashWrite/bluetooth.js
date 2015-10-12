c94cbeda817f81be76743e9e9aa0d20cbc408991
inho <InhoChoi@inhoui-MacBook-Pro.local>
2015-10-12 17:30:36 +0900

    Bluetooth Example 추가

---
+++ FlashWrite/bluetooth.js
@@ -0,0 +1,13 @@
+Serial3.begin(9600);
+pinMode(4,13,1);
+while(1){
+	var ch = Serial3.read();
+	if( ch != 0){
+		Serial3.print(ch);
+	}
+	
+	digitalWrite(4,13,0);
+	Delay(500);
+	digitalWrite(4,13,1);
+	Delay(500);
+}
