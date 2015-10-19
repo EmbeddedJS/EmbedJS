9848d2ecab3fef4ab9529cf9c3f20872fa913e8d
inho <InhoChoi@inhoui-MacBook-Pro.local>
2015-10-19 16:02:55 +0900

    RC car 예제 수정

--- Example/rc.js
+++ Example/rc.js
@@ -11,40 +11,38 @@ pinMode(2, 15, 1);
 while (1) {
     while (Serial3.available()) {
         var ch = Serial3.read();
-        //Forward
-        if (ch == 'w') {
+        if (ch == 'x') {
             digitalWrite(2, 12, 1);
             digitalWrite(2, 13, 0);
             digitalWrite(2, 14, 1);
             digitalWrite(2, 15, 0);
         }
-        //Back
-        if (ch == 's') {
+        if (ch == 'w') {
             digitalWrite(2, 12, 0);
             digitalWrite(2, 13, 1);
             digitalWrite(2, 14, 0);
             digitalWrite(2, 15, 1);
         }
-        //Left
-        if (ch == 'a') {
-            digitalWrite(2, 12, 1);
-            digitalWrite(2, 13, 0);
+        if (ch == 'd') {
+            digitalWrite(2, 12, 0);
+            digitalWrite(2, 13, 1);
             digitalWrite(2, 14, 0);
             digitalWrite(2, 15, 0);
         }
-        //Right
-        if (ch == 'd') {
+        if (ch == 'a') {
             digitalWrite(2, 12, 0);
             digitalWrite(2, 13, 0);
-            digitalWrite(2, 14, 1);
-            digitalWrite(2, 15, 0);
+            digitalWrite(2, 14, 0);
+            digitalWrite(2, 15, 1);
         }
-        Serial3.print(ch);
-        Serial3.print("\n");
-        Delay(100);
-        digitalWrite(2, 12, 0);
-        digitalWrite(2, 13, 0);
-        digitalWrite(2, 14, 0);
-        digitalWrite(2, 15, 0);
+        if (ch == 's') {
+            digitalWrite(2, 12, 0);
+            digitalWrite(2, 13, 0);
+            digitalWrite(2, 14, 0);
+            digitalWrite(2, 15, 0);
+        };
+
     }
+
+
 }
